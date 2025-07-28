47f4a0bd34848a989f91c499e58c882baa70994d
MDAS <localhost>
2025-07-28 13:25:52 -0400

    DISWS3 forgot autosave

--- iocBoot/iocdisws3/st.cmd
+++ iocBoot/iocdisws3/st.cmd
@@ -11,4 +11,25 @@ dbLoadRecords("../../db/linStatNIC.db","IOC=MDAS:SYS:DISWS3,NIC=eno8303")
 dbLoadRecords("../../db/linStatFS.db","P=MDAS:SYS:DISWS3:ROOT,DIR=/")
 dbLoadRecords("../../db/linStatFS.db","P=MDAS:SYS:DISWS3:SEC,DIR=/mnt/secondary")
 
+dbLoadRecords("../../db/save_restoreStatus.db", "P=MDAS:AS:DISWS3:")
+save_restoreSet_status_prefix("MDAS:AS:DISWS3:")
+
+system "install -d as/DISWS3"
+
+set_savefile_path("$(PWD)/as", "/DISWS3")
+set_requestfile_path("$(PWD)/as", "/DISWS3")
+
+set_pass0_restoreFile("atf_settings.sav")
+set_pass0_restoreFile("atf_values.sav")
+set_pass1_restoreFile("atf_values.sav")
+set_pass1_restoreFile("atf_waveforms.sav")
+
 iocInit()
+
+makeAutosaveFileFromDbInfo("$(PWD)/as/DISWS3/atf_settings.req", "autosaveFields_pass0")
+makeAutosaveFileFromDbInfo("$(PWD)/as/DISWS3/atf_values.req", "autosaveFields")
+makeAutosaveFileFromDbInfo("$(PWD)/as/DISWS3/atf_waveforms.req", "autosaveFields_pass1")
+
+create_monitor_set("atf_settings.req", 10 , "")
+create_monitor_set("atf_values.req", 10 , "")
+create_monitor_set("atf_waveforms.req", 30 , "")
